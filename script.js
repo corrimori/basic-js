2ae38fd8639263d7ffe326fe9e2d770c89d76d35
Corrine Morita <corrine@promptt.com>
2018-04-10 12:22:28 -0700

    update to js file

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
 module.exports = {
-  difference: function(){
-
+  difference: function(num1,num2) {
+    return num1 - num2;
   },
-  isEqual: function(){
-
+  isEqual: function(num1,num2) {
+    return num1 === num2;
   },
-  isEven: function(){
-
+  isEven: function(num1){
+    return num1 % 2 === 0;
   },
-  isDivisible: function(){
-
+  isDivisible: function(num1, num2) {
+    return num1 % num2 === 0;
   },
-  discountPercentage: function(){
-
+  discountPercentage: function(amount, percent){
+    if (percent < 0 || percent > 100) {
+        return "please enter a number between 0 and 100";
+    } else {
+        return amount * (percent/100);
+    }
   },
-  isVowel: function(){
-
+  isVowel: function(letter) {
+  //   if (letter = "A" || letter = 'E' || letter = 'I' || letter = 'O' || letter = "U")
+  //     return True;
   },
-  celsiusToFahrenheit: function(){
-
+  celsiusToFahrenheit: function(temp){
+    return (temp * 1.8) + 32;
   },
-  biggestOfThree: function(){
-
+  biggestOfThree: function( num1, num2, num3 ){
+    if (num1 > num2) {
+      if (num1 > num3) {
+        return num1;
+      } else if (num2 > num3) {
+          return num2;
+      } else {
+          return num3;
+      }
+    }
   },
-  // Bonus
-  federalIncomeTaxCalculator: function(){
+//Bonus
+  federalIncomeTaxCalculator: function(salary){
 
   }
 };
